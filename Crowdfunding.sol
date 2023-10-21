e38113a24e9959c4fcb6db6b2d49bdebbc61372b
DevBhojani116 <devbhojani116@gmail.com>
2023-10-21 14:47:08 +0530

    Resolved createCampaign function's error, and wrote the functions for viewing and donating to the campaigns

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -2,43 +2,80 @@ pragma solidity 0.8.19;
 
 contract Crowdfunding
 {
+    constructor() {}
     struct Campaign
     {
-        address creator;
+        address payable creator;
+        uint campaignNumber;
         string cause;
         string futurePlans;
         uint startDate;
         uint endDate;
         uint requiredAmount;
         uint raisedAmount;
-        mapping(address => Funder) funders;
     }
 
     struct Funder
     {
-        address funder;
-        uint donatedAmount;
+        address funderAddress;
+        mapping(uint=>uint) donatedInThis;
+        //campaignID => amount donated in this campaign
     }
 
-    Campaign[] public campaigns;
+    
+    uint campaignID = 0;
+    Campaign[] public allCampaigns; // Array to store all campaigns
+    mapping(address=>Funder) funders;
+
 
     function createCampaign(string memory _cause, string memory _futurePlans, uint _endDate, uint _requiredAmount) public
     {
         require(_requiredAmount>0, "Required amount must be positive");
         require(_endDate>block.timestamp, "Campaign should end after its creation");
 
-        Campaign memory temp = Campaign
+        allCampaigns[campaignID] = 
+        Campaign
         ({
-            creator: msg.sender,
+            creator: payable(msg.sender),
+            campaignNumber: campaignID,
             cause: _cause,
             futurePlans: _futurePlans,
             startDate: block.timestamp,
             endDate: _endDate,
             requiredAmount: _requiredAmount,
             raisedAmount: 0
-            // funders
         });
+        campaignID++;
     }
 
-    
+    modifier notEnded(uint campaignId) 
+    {
+        require(block.timestamp < allCampaigns[campaignId].endDate, "The campaign has ended");
+        _;
+    }
+
+    modifier positiveAmount(uint amount)
+    {
+        require(amount>0, "Amount must be postive");
+        _;
+    }
+
+    function donate(uint _campaignID) public payable notEnded(_campaignID) positiveAmount(msg.value)
+    {
+        require(_campaignID < campaignID, "Invalid Campaign ID");
+        require(address(msg.sender).balance<msg.value, "Insufficient balance in your wallet");
+
+        Funder storage funder = funders[msg.sender];
+        require(funder.donatedInThis[campaignID]>0, "You have already donated to this campaign");
+
+        allCampaigns[_campaignID].raisedAmount += msg.value;
+        allCampaigns[_campaignID].creator.transfer(msg.value);
+        
+        funder.donatedInThis[_campaignID] += msg.value;
+    }
+
+    function viewCampaigns() public view returns(Campaign[] memory)
+    {
+        return allCampaigns;
+    }
 }
