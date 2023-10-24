43c053e1a986796f4ad50b6bd1c4f642b873aedc
DevBhojani116 <devbhojani116@gmail.com>
2023-10-24 20:52:12 +0530

    Rectified the bugs in the contract and deployed it again

--- Crowdfunding.sol
+++ Crowdfunding.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.x;
 
 contract Crowdfunding
 {
+
     struct Campaign
     {
         address payable creator;
@@ -25,15 +26,14 @@ contract Crowdfunding
     
     uint campaignID = 0;
     Campaign[] public allCampaigns; // Array to store all campaigns
-    mapping(address=>Funder) funders;
-
+    mapping(address=>Funder) funders; //this mapping is to track each funder using their address
 
     function createCampaign(string memory _cause, string memory _futurePlans, uint _endDate, uint _requiredAmount) external
     {
         require(_requiredAmount>0, "Required amount must be positive");
         require(_endDate>block.timestamp, "Campaign should end after its creation");
 
-        allCampaigns[campaignID] = 
+        allCampaigns.push( 
         Campaign
         ({
             creator: payable(msg.sender),
@@ -44,34 +44,63 @@ contract Crowdfunding
             endDate: _endDate,
             requiredAmount: _requiredAmount,
             raisedAmount: 0
-        });
+        }));
         campaignID++;
     }
 
+    //checks whether the time stated by the creator has passed or can anyone still donate in it
     modifier notEnded(uint campaignId) 
     {
         require(block.timestamp < allCampaigns[campaignId].endDate, "The campaign has ended");
         _;
     }
 
+    //checks whether the amount required for the campaign has already been raised or can anyone still donate to raise the amount
+    modifier requiredAmountFulfilled(uint _campaignID)
+    {
+        require(allCampaigns[_campaignID].raisedAmount<allCampaigns[_campaignID].requiredAmount, "Creator's amount requirement already fulfilled");
+        _;
+    }
+
+    //Donor should enter positive amount
     modifier positiveAmount(uint amount)
     {
         require(amount>0, "Amount must be postive");
         _;
     }
 
-    function donate(uint _campaignID) external payable notEnded(_campaignID) positiveAmount(msg.value)
+    //checks whether the any campaign exists with the entered campaign ID or not
+    modifier validity(uint _campaignID)
     {
         require(_campaignID<campaignID, "Invalid Campaign ID");
+        _;
+    }
+
+    //donate in Wei
+    function donate(uint _campaignID) external payable notEnded(_campaignID) positiveAmount(msg.value) validity(_campaignID) requiredAmountFulfilled(_campaignID) returns(string memory)
+    {
         require(address(msg.sender).balance>=msg.value, "Insufficient balance in your wallet");
 
         Funder storage funder = funders[msg.sender];
+
         require(funder.donatedInThis[_campaignID]==0, "You have already donated to this campaign");
 
-        allCampaigns[_campaignID].raisedAmount += msg.value;
-        allCampaigns[_campaignID].creator.transfer(msg.value);
-        
-        funder.donatedInThis[_campaignID] += msg.value;
+        //if the donor is trying to send more money than required then this "if" block will be executed
+        if(msg.value+allCampaigns[_campaignID].raisedAmount>allCampaigns[_campaignID].requiredAmount)
+        {
+            uint amountToBeTransferred = allCampaigns[_campaignID].requiredAmount-allCampaigns[_campaignID].raisedAmount;
+            allCampaigns[_campaignID].creator.transfer(amountToBeTransferred);
+            allCampaigns[_campaignID].raisedAmount = allCampaigns[_campaignID].requiredAmount;
+            funder.donatedInThis[_campaignID] += amountToBeTransferred;
+            return ("Creator's amount requirement already fulfilled so transferred only the remaining amount");
+        }
+        else
+        {
+            allCampaigns[_campaignID].raisedAmount += msg.value;
+            allCampaigns[_campaignID].creator.transfer(msg.value);
+            funder.donatedInThis[_campaignID] += msg.value;
+            return ("Donated");
+        }
     }
 
     function viewCampaigns() public view returns(Campaign[] memory)
